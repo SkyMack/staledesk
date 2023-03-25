a4814cc8ea8c08c62df945832f02cd8988e321bd
Devon Mackay <devon.g.mackay@gmail.com>
2023-03-25 14:51:54 -0500

    Add new helper function

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,5 +1,10 @@
 package controller
 
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
 type ErrorResp struct {
 	Description string         `json:"description"`
 	Errors      []ErrorDetails `json:"errors"`
@@ -9,4 +14,13 @@ type ErrorDetails struct {
 	Field   string `json:"field"`
 	Message string `json:"message"`
 	Code    string `json:"code"`
+}
+
+// randHexString generates a random hex value of length `n`
+func randHexString(n int) (string, error) {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
 }
